012c9afdfdaf613caaad3997cd1428e17bc133a1
David Leder <davidleder98@gmail.com>
2024-07-19 18:59:02 +0200

    Hard coded 4 products, updated view

--- BasicOnlineStore/Controllers/ProductController.cs
+++ BasicOnlineStore/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BasicOnlineStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BasicOnlineStore.Controllers
 {
@@ -6,7 +7,14 @@ namespace BasicOnlineStore.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<ProductModel> productsList = new List<ProductModel>();
+
+            productsList.Add(new ProductModel { Id = 1, Name = "Ooga", Price = 11.11m, Description = "Booga"});
+            productsList.Add(new ProductModel { Id = 2, Name = "Jeffery", Price = 22.11m, Description = "Epstein" });
+            productsList.Add(new ProductModel { Id = 3, Name = "I like", Price = 11.31m, Description = "cheese" });
+            productsList.Add(new ProductModel { Id = 4, Name = "I dont like", Price = 88.88m, Description = "sand" });
+
+            return View(productsList);
         }
     }
 }
