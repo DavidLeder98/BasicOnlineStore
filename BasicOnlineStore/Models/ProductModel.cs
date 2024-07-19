a54e59450b309eae0abeb192012ec6cd99ae585f
David Leder <davidleder98@gmail.com>
2024-07-19 19:02:18 +0200

    Updated product index

--- BasicOnlineStore/Models/ProductModel.cs
+++ BasicOnlineStore/Models/ProductModel.cs
@@ -1,10 +1,19 @@
-﻿namespace BasicOnlineStore.Models
+﻿using System.ComponentModel;
+
+namespace BasicOnlineStore.Models
 {
     public class ProductModel
     {
+        [DisplayName("Id number")]
         public int Id { get; set; }
+
+        [DisplayName("Product")]
         public string Name { get; set; }
+
+        [DisplayName("Price")]
         public decimal Price { get; set; }
+
+        [DisplayName("Description")]
         public string Description { get; set; }
     }
 }
