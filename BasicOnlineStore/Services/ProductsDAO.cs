d3beeba749f9fce8f889fe79f2d3205c3fcfabdf
David Leder <davidleder98@gmail.com>
2024-07-20 11:32:48 +0200

    Made database with randomly generated data, made interface to use data

---
+++ BasicOnlineStore/Services/ProductsDAO.cs
@@ -0,0 +1,37 @@
+﻿using BasicOnlineStore.Models;
+
+namespace BasicOnlineStore.Services
+{
+    public class ProductsDAO : IProductDataService
+    {
+        public List<ProductModel> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductModel> SearchProducts(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductModel GetProductById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(ProductModel product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(ProductModel product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(ProductModel product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
